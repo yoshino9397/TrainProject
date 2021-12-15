a92fe85a8b908ae3a32464107184c97d6e006a3b
Yoshino <88402243+yoshino9397@users.noreply.github.com>
2021-12-14 16:30:48 -0800

    html&JS

---
+++ JS/cgs.js
@@ -0,0 +1,52 @@
+function off() {
+  document.getElementById("mask").style.opacity = "0";
+}
+
+function modal() {
+  document.getElementById("modal1").style.opacity = "1";
+  document.getElementById("mask").style.opacity = "1";
+}
+function modaloff() {
+  // document.getElementById("mask").style.opacity = "0";
+  document.getElementById("modal1").style.opacity = "0";
+}
+function modal2() {
+  document.getElementById("modal2").style.opacity = "1";
+  document.getElementById("mask").style.opacity = "1";
+}
+function modaloff2() {
+  // document.getElementById("mask").style.opacity = "0";
+  document.getElementById("modal2").style.opacity = "0";
+}
+function modal3() {
+  document.getElementById("modal3").style.opacity = "1";
+  document.getElementById("mask").style.opacity = "1";
+}
+function modaloff3() {
+  // document.getElementById("mask").style.opacity = "0";
+  document.getElementById("modal3").style.opacity = "0";
+}
+function modal4() {
+  document.getElementById("modal4").style.opacity = "1";
+  document.getElementById("mask").style.opacity = "1";
+}
+function modaloff4() {
+  // document.getElementById("mask").style.opacity = "0";
+  document.getElementById("modal4").style.opacity = "0";
+}
+function modal5() {
+  document.getElementById("modal5").style.opacity = "1";
+  document.getElementById("mask").style.opacity = "1";
+}
+function modaloff5() {
+  // document.getElementById("mask").style.opacity = "0";
+  document.getElementById("modal5").style.opacity = "0";
+}
+function modal6() {
+  document.getElementById("modal6").style.opacity = "1";
+  document.getElementById("mask").style.opacity = "1";
+}
+function modaloff6() {
+  // document.getElementById("mask").style.opacity = "0";
+  document.getElementById("modal6").style.opacity = "0";
+}
