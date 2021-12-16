9b20c2425e3197259c589b2b283f93474784f31c
yoshino9397 <yayamayoshino@gmail.com>
2021-12-15 22:36:49 -0800

    Fin

---
+++ JS/special.js
@@ -0,0 +1,8 @@
+function jpSpecial(){
+  document.getElementById("main").style.opacity = "1";
+  document.getElementById("en").style.opacity = "0";
+}
+function enSpecial(){
+  document.getElementById("main").style.opacity = "0";
+  document.getElementById("en").style.opacity = "1";
+}
