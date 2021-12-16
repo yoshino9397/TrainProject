f513841693e36b851d8e68f74bb9d1b9a51ffa69
yoshino9397 <yayamayoshino@gmail.com>
2021-12-15 20:30:19 -0800

    eng

---
+++ JS/story.js
@@ -0,0 +1,8 @@
+function jp(){
+  document.getElementById("jp").style.opacity = "1";
+  document.getElementById("en").style.opacity = "0";
+}
+function en(){
+  document.getElementById("jp").style.opacity = "0";
+  document.getElementById("en").style.opacity = "1";
+}
