9d1f3c1137e0a304fe2ed78b4d1111757e259469
yoshino9397 <yayamayoshino@gmail.com>
2021-12-15 21:28:21 -0800

    supportEng

---
+++ JS/support.js
@@ -0,0 +1,8 @@
+function jpSupport(){
+  document.getElementById("main").style.opacity = "1";
+  document.getElementById("en").style.opacity = "0";
+}
+function enSupport(){
+  document.getElementById("main").style.opacity = "0";
+  document.getElementById("en").style.opacity = "1";
+}
